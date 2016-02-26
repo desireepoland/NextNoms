1126061ca92e7fe981ff3d55c17f280feda5a4eb
Desiree <desireepoland@gmail.com>
2016-02-26 10:10:55 -0800

    add user method for find_or_create_from_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  def self.find_or_create_from_omniauth(auth_hash)
+    user = self.find_by(uid: auth_hash["uid"])
+    if !user.nil?
+      return user
+    else
+      user = User.new
+      user.uid = auth_hash["uid"]
+      user.name = auth_hash["info"]["name"]
+      user.provider   = auth_hash["provider"]
+      user.image_url = auth_hash["info"]["image"]
+      if user.save
+        user
+      else
+        nil
+      end
+    end
+  end
 end
