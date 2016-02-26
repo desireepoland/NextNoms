75d004048196e5fedb004bece7433d40fefaf083
Desiree <desireepoland@gmail.com>
2016-02-25 17:43:21 -0800

    Set up sessions controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+  # The helper_method line allows us to use @current_user in our view files.
+
+  def require_login
+    unless current_user
+      flash[:error] = "Please log in."
+      redirect_to login_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new; end
+
+  def create
+  auth_hash = request.env['omniauth.auth']
+  if auth_hash["uid"]
+    @user = User.find_or_create_from_omniauth(auth_hash)
+    if @user
+      session[:user_id] = @user.id
+    else
+      flash[:notice] = "Failed to save the user"
+    end
+  else
+    flash[:notice] = "Failed to authenticate"
+  end
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
 end
