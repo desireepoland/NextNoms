f329722eef90b791366a94eec2fe4585c4e5f7cd
Desiree <desireepoland@gmail.com>
2016-02-29 11:07:14 -0800

    Set up for tried and active separation of restaurants

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,7 @@
 class SiteController < ApplicationController
   def index
-    @restaurants = current_user.restaurants if current_user
+    @active_restaurants = current_user.restaurants.where(users_restaurants: {tried: false}) if current_user
+    @tried_restaurants = current_user.restaurants.where(users_restaurants: {tried: true}) if current_user
     @key = ENV["GOOGLE_API_KEY"]
   end
 end
