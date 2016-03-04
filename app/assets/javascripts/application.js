aec3cf5606469135c4a9b5c8fd5691f5beef1736
Desiree <desireepoland@gmail.com>
2016-03-04 13:24:37 -0800

    Add navbar template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,4 +27,17 @@ $(document).ready(function() {
     document.cookie = ('filter='+ $(this).data('filter'));
     window.location.reload();
   });
+
+// navbar js
+  var menuToggle = $('#js-mobile-menu').unbind();
+   $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+       $('#js-navigation-menu').removeAttr('style');
+     }
+   });
+ });
 });
