64cfd09397a302581e5e32a3bf7a4dfb0c0a49c5
Desiree <desireepoland@gmail.com>
2016-03-03 18:42:51 -0800

    Order list by active vs tried

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,6 @@
 class SiteController < ApplicationController
   def index
-    @restaurants = current_user.restaurants if current_user
+    @restaurants = current_user.restaurants.order("users_restaurants.tried ASC") if current_user
     @key = ENV["GOOGLE_API_KEY"]
   end
 end
