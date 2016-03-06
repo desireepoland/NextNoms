c91e9449e9dd1931ea5fe7d75341456c89e849f0
Desiree <desireepoland@gmail.com>
2016-03-05 23:15:20 -0800

    set default cookies for filter toggle

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :current_user
+  before_action :current_user, :set_default_cookies
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   helper_method :logged_in?
   helper_method :current_user
+
+  def set_default_cookies
+    cookies[:filter] ||= 'all'
+  end
 
   def require_login
     unless current_user
