10f25ce8aa59668c82fbfcb9e45c32bf6b5551a3
Desiree <desireepoland@gmail.com>
2016-03-09 14:16:13 -0800

    Add spec for restaurant create method

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -1,23 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe RestaurantsController, type: :controller do
-  # let (:user) { create(:user) }
-  # let (:restaurant) { create(:restaurant) }
-  #
-  # before :each do
-  #   @restaurant = Restaurant.create(place_id: "1234")
-  # end
-  #
-  # let(:create_params) do
-  #   { restaurant: { place_id: "1234"}}
-  # end
+  let (:user) { create(:user) }
+  let (:restaurant) { create(:restaurant) }
+
+  let(:create_params) do
+    { user_id: 1,
+      restaurant: {
+        place_id: "54321"
+      }
+    }
+  end
 
   describe "POST #create" do
-    # it "redirects to the root path" do
-    #   post :create, ....
-    #   expect(response.status).to eq 302
-    #   expect(subject).to redirect_to root_path
-    # end
+    it "redirects to the root path" do
+      post :create, create_params, user_id: user.id
+      expect(response.status).to eq 302
+      expect(subject).to redirect_to root_path
+    end
+    it "adds a new restaurant to the user" do
+      @restaurants = user.restaurants.all.count
+      post :create, create_params, user_id: user.id
+      expect(user.restaurants.all.count).to eq(@restaurants + 1)
+    end
   end
 
 
