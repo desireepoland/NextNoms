c3198596afd395e7926ccc8e900ccdb370e39de3
Desiree <desireepoland@gmail.com>
2016-03-11 21:42:51 -0800

    Add specs

--- spec/controllers/identities_controller_spec.rb
+++ spec/controllers/identities_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe IdentitiesController, type: :controller do
-
+  describe "new" do
+    it "renders new template" do
+      get :new
+      expect(response.status).to eq 200
+    end
+  end
 end
--- spec/controllers/site_controller_spec.rb
+++ spec/controllers/site_controller_spec.rb
@@ -4,12 +4,28 @@ RSpec.describe SiteController, type: :controller do
   let (:user) { create(:user) }
 
   describe "GET 'index'" do
-    it "is successful when logged in" do
-      user = User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:facebook])
-      session[:user_id] = user.id
-      get :index
-      expect(response.status).to eq 200
-      expect(subject).to render_template :index
+    context "when logged in" do
+      before do
+        user = User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:facebook])
+        session[:user_id] = user.id
+      end
+      it "is successful when logged in" do
+        get :index
+        expect(response.status).to eq 200
+        expect(subject).to render_template :index
+      end
+      it "filters for active" do
+        cookies[:filter] = "active"
+        get :index
+        expect(response.status).to eq 200
+        expect(subject).to render_template :index
+      end
+      it "filters for tried" do
+        cookies[:filter] = "tried"
+        get :index
+        expect(response.status).to eq 200
+        expect(subject).to render_template :index
+      end
     end
     it "is successful when not logged in" do
       get :index
