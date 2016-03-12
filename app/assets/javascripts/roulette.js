5b7c7dfec73a5566178c4111012a1806ed96faaf
Desiree <desireepoland@gmail.com>
2016-03-12 15:35:47 -0800

    Center map on restaurant, remove unneccessary geolocator

--- app/assets/javascripts/roulette.js
+++ app/assets/javascripts/roulette.js
@@ -2,30 +2,13 @@ var rMap;
 function initRouletteMap() {
   var pos;
   var rMap = new google.maps.Map(document.getElementById('r-map'), {
-    center: pos,
     zoom: 13
   });
 
-  //HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      rMap.setCenter(pos);
-    }, function() {
-      handleLocationError(true, infoWindow, rMap.getCenter());
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, rMap.getCenter());
-  }
-
-  // set place_id to the current roulette restaurant's place_id
+  var placeId = $('#r-choice').data('place-id');
   var request = {
-        placeId: $('#r-choice').data('place-id')
-    };
+    placeId: placeId
+  };
 
   var infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(rMap);
@@ -41,15 +24,13 @@ function initRouletteMap() {
 
             infowindow.setContent(place.name);
             infowindow.open(rMap, marker);
+            rMap.setCenter(place.geometry.location);
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(place.name);
+                infowindow.open(rMap, this);
+            });
         }
     });
 
-}
-
-//geolocation error handling
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
 }
