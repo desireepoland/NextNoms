08397e73534096cf9f755d3c1ff013fd657f7c5e
Desiree <desireepoland@gmail.com>
2016-03-16 13:43:30 -0700

    change ordering of restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ActiveRecord::Base
   has_many :users, :through => :users_restaurants
   has_many :users_restaurants
 
-  scope :ordered, -> { order("users_restaurants.tried ASC") }
+  scope :ordered, -> { order("users_restaurants.tried ASC, users_restaurants.created_at DESC") }
   self.per_page = 10
 end
